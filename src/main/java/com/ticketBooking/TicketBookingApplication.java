40e171edf73475d88149e24d6e9a3c23f2339fde
Ayush <ayush1967@gmail.com>
2025-07-14 07:51:49 +0530

    Initial commit: Ticket Booking System backend in Spring Boot

---
+++ src/main/java/com/ticketBooking/TicketBookingApplication.java
@@ -0,0 +1,14 @@
+package com.ticketBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HEllo and welcome to my Project!!!!");
+		SpringApplication.run(TicketBookingApplication.class, args);
+	}
+
+}
